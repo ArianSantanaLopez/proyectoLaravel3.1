d7d46eb5ca33dfbe35e5c607400483fffba3184f
ivan10reyes <ivanreyesbaeza@alumno.ieselrincon.es>
2026-03-06 17:31:31 +0000

    Implementación autenticación con tokens Sanctum y frontend API

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\PartidoController;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::prefix('v1')->group(function () {
+Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
 
-    Route::apiResource('partidos', PartidoController::class);
+Route::apiResource('partidos', PartidoController::class);
 
 });
+Route::post('/login',[AuthController::class,'login']);
